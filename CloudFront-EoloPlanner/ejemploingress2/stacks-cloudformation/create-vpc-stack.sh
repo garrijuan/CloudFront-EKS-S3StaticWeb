5d706ef84ff79ce1b87c2e45ef5226378c389a8e
garrijuan <garrijuan@gmail.com>
2023-10-26 17:30:47 +0200

    new example ingress

---
+++ CloudFront-EoloPlanner/ejemploingress2/stacks-cloudformation/create-vpc-stack.sh
@@ -0,0 +1,4 @@
+aws cloudformation create-stack \
+  --region us-east-1 \
+  --stack-name my-eks-vpc \
+  --template-body file:///home/juanangel/Escritorio/TFM/CloudFront-EKS-S3StaticWeb/CloudFront-EoloPlanner/ejemploingress2/stacks-cloudformation/vpc-stack.yaml 
